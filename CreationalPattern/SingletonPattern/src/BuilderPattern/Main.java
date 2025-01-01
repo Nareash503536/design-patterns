182989bda72385c1a68ad8a8b5e3aa4b257a1dd3
V.NARESHKAAN <nareash20010150@gmail.com>
2025-01-01 12:54:23 +0530

    Builder pattern

--- CreationalPattern/SingletonPattern/src/BuilderPattern/Main.java
+++ CreationalPattern/SingletonPattern/src/BuilderPattern/Main.java
@@ -1,7 +1,80 @@
 package BuilderPattern;
 
+class Computer {        // Product
+    private String cpu;
+    private String ram;
+    private String storage;
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    public void displayInfo(){
+        System.out.println("CPU: " + cpu);
+        System.out.println("RAM: " + ram);
+        System.out.println("Storage: " + storage);
+    }
+}
+
+interface Builder {
+    void buildCPU();
+    void buildRAM();
+    void buildStorage();
+    Computer build();
+}
+
+class GamingComputerBuilder implements Builder {
+    private Computer computer;
+
+    public GamingComputerBuilder() {
+        this.computer = new Computer();
+    }
+
+    @Override
+    public void buildCPU() {
+        computer.setCpu("Gaming CPU");
+    }
+
+    @Override
+    public void buildRAM() {
+        computer.setRam("16GB DDR4");
+    }
+
+    @Override
+    public void buildStorage() {
+        computer.setStorage("1TB SSD");
+    }
+
+    @Override
+    public Computer build() {
+        return computer;
+    }
+}
+
+class ComputerDirector {
+    public void construct(Builder builder) {
+        builder.buildCPU();
+        builder.buildRAM();
+        builder.buildStorage();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        GamingComputerBuilder gamingComputerBuilder = new GamingComputerBuilder();
+        ComputerDirector computerDirector = new ComputerDirector();
 
+        computerDirector.construct(gamingComputerBuilder);
+        Computer gamingComputer = gamingComputerBuilder.build();
+
+        gamingComputer.displayInfo();
     }
 }
